2ef8b45f466f0b25e3047d340f84eda3e859cfc8
DrubRoxas <manuel9.acci@gmail.com>
2019-09-26 12:48:55 +0200

    1

--- DAWEB/Matrices/Index.php
+++ DAWEB/Matrices/Index.php
@@ -1,46 +1,40 @@
 <?php
-    require_once'../../PHP20192020/Cargadores/cargarclases.php';
+require_once '../../PHP20192020/Cargadores/cargarclases.php';
 
-    class Principal
+class Principal
+{
+    public static function main()
     {
-        public static function main()
-        {
-            //Recorrer Matrices
-            //Unidimensional
-            foreach (EjemplosMatrices::$nombres as $key => $value) 
-            {
-                echo $key."...".$value."<br>";
-            }
+        //Recorrer Matrices
+        //Unidimensional
+        foreach (EjemplosMatrices::$nombres as $key => $value) {
+            echo $key . "..." . $value . "<br>";
+        }
 
-            //Asociativa
-            foreach (EjemplosMatrices::$NombreEdad as $nombre => $edad) 
-            {
-                echo "el tipo".$nombre."tiene ".$edad."años <br>";
-            }
+        //Asociativa
+        foreach (EjemplosMatrices::$NombreEdad as $nombre => $edad) {
+            echo "el tipo" . $nombre . "tiene " . $edad . "años <br>";
+        }
 
-            //Elementos al final del array
-            EjemplosMatrices::$edades[100]=5;
-            EjemplosMatrices::$edades[]=89;
+        //Elementos al final del array
+        EjemplosMatrices::$edades[100] = 5;
+        EjemplosMatrices::$edades[] = 89;
 
-            //Muestra la variable con toda su informacion (Se utiliza paraa depurar)
-            var_dump(EjemplosMatrices::$edades);
+        //Muestra la variable con toda su informacion (Se utiliza paraa depurar)
+        var_dump(EjemplosMatrices::$edades);
 
-            //Recorrer Matriz de objetos asociativa
-            foreach (EjemplosMatrices::MatrizObjetosAsociativa() as $nombre => $animal) 
-            {
-                echo "El animal con nombre ".$nombre." Pertenece al género ".$animal->getGenero()."<br>";
-            }
+        //Recorrer Matriz de objetos asociativa
+        foreach (EjemplosMatrices::MatrizObjetosAsociativa() as $nombre => $animal) {
+            echo "El animal con nombre " . $nombre . " Pertenece al género " . $animal->getGenero() . "<br>";
+        }
 
-            //Matriz de 2 dimensiones
-            foreach (EjemplosMatrices::$comunidadesasoc as $comunidad => $matrizprovincias)
-            {
-                echo "<br>la comunidad de ".$comunidad." Tiene las siguientes provincias:<br>";
-                foreach ($matrizprovincias as $provincia)
-                {
-                    echo "$provincia<br>";
-                }
+        //Matriz de 2 dimensiones
+        foreach (EjemplosMatrices::$comunidadesasoc as $comunidad => $matrizprovincias) {
+            echo "<br>la comunidad de " . $comunidad . " Tiene las siguientes provincias:<br>";
+            foreach ($matrizprovincias as $provincia) {
+                echo "$provincia<br>";
             }
         }
     }
-    principal::main();
-?>
+}
+principal::main();
