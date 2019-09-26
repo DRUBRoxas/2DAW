4c9b2207f07a35bc800d54590d7eceb8569ba12e
DrubRoxas <manuel9.acci@gmail.com>
2019-09-26 13:13:54 +0200

    1

--- DAWEB/Veterinaria/Vistas/Principal/enruta.php
+++ DAWEB/Veterinaria/Vistas/Principal/enruta.php
@@ -1,9 +1,9 @@
 <?php
 if (isset($_GET['menu'])) {
     if ($_GET['menu'] == "mantenimiento") {
-        require_once '../Mantenimiento/mantenimiento.php';
+        require_once './Vistas/Mantenimiento/mantenimiento.php';
     }
     if ($_GET['menu'] == "listados") {
-        require_once '../Mantenimiento/Listado.php';
+        require_once './Vistas/Mantenimiento/Listado.php';
     }
 }
--- DAWEB/Veterinaria/Vistas/Principal/layout.php
+++ DAWEB/Veterinaria/Vistas/Principal/layout.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SANIMAL</title>
-    <link rel="stylesheet" href="../../css/estilos.css">
+    <title>SANIMAL S.L.</title>
+    <link rel="stylesheet" href="./css/estilos.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
@@ -22,18 +22,18 @@
 
 <body>
     <?php
-        require_once './header.php';
+        require_once './Vistas/Principal/header.php';
     ?>
     <section>
         <div id="cuerpo">
         <?php
-           require_once './enruta.php';
+           require_once './Vistas/Principal/enruta.php';
         ?>
         </div>
     </section>
 
     <?php
-        require_once './footer.php';
+        require_once './Vistas/Principal/footer.php';
     ?>
 
 </body>
