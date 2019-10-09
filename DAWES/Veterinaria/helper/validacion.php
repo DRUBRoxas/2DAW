18e8b09ce00babe306e25f92561312ff4f8018fb
DrubRoxas <manuel9.acci@gmail.com>
2019-10-09 13:14:39 +0200

    09/10/2019 13:14

--- DAWES/Veterinaria/helper/validacion.php
+++ DAWES/Veterinaria/helper/validacion.php
@@ -7,7 +7,7 @@ class Validacion
     //Constructor
     public function __construct()
     {
-        $this->errores=array();
+        $this->errores = array();
     }
 
     /**
@@ -15,15 +15,54 @@ class Validacion
      *
      * @param [type] $campo
      * @return boolean
-     */ 
+     */
     public function Requerido($campo)
     {
-        if(!isset($_POST[$campo]) || empty($_POST[$campo]))
-        {
-            $this->errores[$campo]="El campo $campo no puede estar vacio";
+        if (!isset($_POST[$campo]) || empty($_POST[$campo])) {
+            $this->errores[$campo] = "El campo $campo no puede estar vacio";
             return false;
         }
         return true;
     }
-     
+
+    public function EnteroRango($campo, $min = PHP_INT_MIN, $max = PHP_INT_MAX)
+    {
+        if ($campo > $max && $campo < $min) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function CadenaRango($campo, $max, $min = 0)
+    {
+        if (strlen($campo) < $max && strlen($campo) > $min) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function Email($campo)
+    {
+        if (filter_var($campo, FILTER_VALIDATE_EMAIL)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function DNI($campo)
+    {
+        function validar_dni($campo)
+        {
+            $letra = substr($campo, -1);
+            $numeros = substr($campo, 0, -1);
+            if (substr("TRWAGMYFPDXBNJZSQVHLCKE", $numeros % 23, 1) == $letra && strlen($letra) == 1 && strlen($numeros) == 8) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
---
+++ DEWEC/3 en Raya/3enraya.js
@@ -0,0 +1,83 @@
+
+function TresEnRaya(p1,p2)
+{
+    this.p1=p1;
+    this.p2=p2;
+    this.juego=[[],[],[]];
+    this.turno=Math.round(Math.random());
+    this.ganador="";
+}
+
+TresEnRaya.prototype.jugar=function(f,c)
+{
+    if(this.ganador=="" && this.juego[f][c]===undefined)
+    {
+        document.getElementById("p"+f+c).innerHTML=(this.turno==1)?"X":"O";
+        this.juego[f][c]=this.turno;
+        this.turno=(this.turno+1)%2;
+        if(document.getElementById("p"+f+c)!==undefined)
+        {
+            this.victoria();
+        }
+    }
+};
+TresEnRaya.prototype.victoria=function(){
+    //codigo victoria
+    //return true/false
+    var j=this.juego;
+    if((j[0][0]+j[0][1]+j[0][2])%3==0 ||
+       (j[1][0]+j[1][1]+j[1][2])%3==0 ||
+       (j[2][0]+j[2][1]+j[2][2])%3==0 ||
+       (j[0][0]+j[1][0]+j[2][0])%3==0 ||
+       (j[0][1]+j[1][1]+j[2][1])%3==0 ||
+       (j[0][2]+j[1][2]+j[2][2])%3==0 ||
+       (j[0][0]+j[1][1]+j[2][2])%3==0 ||
+       (j[2][0]+j[1][1]+j[0][2])%3==0)
+    {
+        if(this.turno==1)
+        {
+            this.ganador=this.p1;
+        }
+        else
+        {
+            this.ganador=this.p2;
+        }
+        alert("FELICIDADES "+this.ganador);
+    }
+
+
+
+};
+
+var partida; //juego
+
+function comenzar(){
+    var form=document.forms["jugadores"]
+    var j1=form["jug1"];
+    var j2=form["jug2"];
+    var i=0, j=0;
+    var contabla="";
+
+    partida=new TresEnRaya(j1.value,j2.value);
+    if(partida.turno===0)
+    {
+        j1.style.border="1px solid green"
+        j2.style.border="none"
+    }else
+    {
+        j1.style.border="1px solid green"
+        j2.style.border="1px solid green"
+    }
+    tbody=document.getElementById("juego");
+    
+    for (i=0;i<3;i++)
+    {
+        contabla+="<tr>"
+        for (j=0;j<3;j++)
+        {
+            contabla+="<td id='p"+i+j+"' onclick='partida.jugar(" +i+ "," +j+ ")'>&nbsp;</td>"
+        }
+        contabla+="</tr>"
+    }
+    tbody.innerHTML=contabla;
+}
